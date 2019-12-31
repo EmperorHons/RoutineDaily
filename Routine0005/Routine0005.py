faead901a6c0397d0b583985f4bece45d8b4cd28
Allen <ysk19950809@163.com>
2019-12-31 11:15:56 +0800

    fifth commit 统计纯英文文本单词出现个数

--- Routine0005/Routine0005.py
+++ Routine0005/Routine0005.py
@@ -3,7 +3,6 @@
 https://blog.csdn.net/shifanfashi/article/details/89394026
 第 0005 题： 你有一个目录，装了很多照片，把它们的尺寸变成都不大于 iPhone5 分辨率的大小
 """
-
 from PIL import Image
 # PIL是Python Imaging Library 是图像存档和批处理应用程序的理想选择。您可以使用该库创建缩略图，在文件格式之间进行转换，打印图像等。
 import os.path
